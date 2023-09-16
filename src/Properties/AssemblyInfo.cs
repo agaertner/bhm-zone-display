b84712b497fda577bd928ae3b42dd37b4e1227f0
GitHub Action <action@github.com>
2023-09-16 19:13:30 +0000

    (skip): 🔖 bump version to 0.8.2-dev.40

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.2.38")]
-[assembly: AssemblyFileVersion("0.8.2.38")]
-[assembly: AssemblyInformationalVersion("0.8.2-dev.38+Branch.dev.Sha.3532fbb5ad365a9b2744aa627ee0dd77888f6392")]
+[assembly: AssemblyVersion("0.8.2.40")]
+[assembly: AssemblyFileVersion("0.8.2.40")]
+[assembly: AssemblyInformationalVersion("0.8.2-dev.40+Branch.dev.Sha.1538bc0c639d1d576b7b96f9c41814ddad6e6f3d")]
